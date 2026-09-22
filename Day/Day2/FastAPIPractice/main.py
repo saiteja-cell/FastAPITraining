3f21ab86fa33c4c87671429c5581582b3854f5db
SAI TEJA <2025cs_bussagounisaiteja_b@nie.ac.in>
2026-09-22 13:20:40 +0530

    Path parameters

--- Day/Day2/FastAPIPractice/main.py
+++ Day/Day2/FastAPIPractice/main.py
@@ -13,3 +13,7 @@ def health():
 @app.post("/create")
 def create_something():
     return {"Message": "Created"}
+#path parameters 
+@app.get("/student/{usn}")
+def get_result(usn):
+    return {"Result":"Distinction","USN":usn}
