1c817fb254ae9f605fa3ca729e5af5a1d52bb2af
SAI TEJA <2025cs_bussagounisaiteja_b@nie.ac.in>
2026-09-22 15:19:21 +0530

    Pydantic model

--- Day/Day2/FastAPIPractice/main.py
+++ Day/Day2/FastAPIPractice/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 @app.get("/")
 def read_root():
@@ -13,11 +14,22 @@ def health():
 @app.post("/create")
 def create_something():
     return {"Message": "Created"}
+
 #path parameters 
 @app.get("/student/{usn}")
 def get_result(usn):
     return {"Result":"Distinction","usn":usn}
+
 #path parameters with Type Hint
 @app.get("/candidate/{rollno}")
 def get_candidate(rollno:int):
     return {"Result":"Distinction","rollno":rollno,"type":str(type(rollno))}
+
+#PYdantic model
+class Item(BaseModel):
+    name:str
+    price:float
+    in_stock:bool =True
+@app.post("/items")
+def create_item(item:Item):
+    return {"received":item,"total_price":item.price*1.18}
