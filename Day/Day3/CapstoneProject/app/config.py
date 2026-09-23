3b8273275f543eb8f86c84712f9ec31ea2a4eb45
SAI TEJA <2025cs_bussagounisaiteja_b@nie.ac.in>
2026-09-23 12:58:54 +0530

    cp:1

---
+++ Day/Day3/CapstoneProject/app/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+
+    MONGO_URI: str = "mongodb://localhost:27017"
+    MONGO_DB_NAME: str = "it_servicedesk"
+
+    APP_NAME: str = "IT_Service Desk App API"
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
+
+
+settings = Settings()
